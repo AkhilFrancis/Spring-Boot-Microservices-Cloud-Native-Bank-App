0426810f586358a27993e490280a18d16af3b54f
Akhil Francis <akhilfrancis0007@gmail.com>
2026-02-28 00:12:17 +0530

    Implement fallback mechanisms for Loans and Cards services; update Feign clients and application configurations for circuit breaker support

--- accounts/src/main/java/com/akhilfrancis/accounts/service/CustomerServiceImpl.java
+++ accounts/src/main/java/com/akhilfrancis/accounts/service/CustomerServiceImpl.java
@@ -37,10 +37,14 @@ public class CustomerServiceImpl implements ICustomersService {
         customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(account, new AccountsDto()));
 
         ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(correlationID, mobileNumber);
-        customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+        if(null != loansDtoResponseEntity) {
+            customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+        }
 
         ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(correlationID, mobileNumber);
-        customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+        if(null != cardsDtoResponseEntity) {
+            customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+        }
 
         return customerDetailsDto;
     }
---
+++ accounts/src/main/java/com/akhilfrancis/accounts/service/client/CardsFallback.java
@@ -0,0 +1,13 @@
+package com.akhilfrancis.accounts.service.client;
+
+import com.akhilfrancis.accounts.dto.CardsDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CardsFallback implements CardsFeignClient{
+    @Override
+    public ResponseEntity<CardsDto> fetchCardDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+}
